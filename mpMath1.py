15a6d2743aa5b506a95ed6fe7cccea401edcca1f
Ahmad Lutfi <obzajd@gmail.com>
2023-04-26 04:39:20 -0400

    Add: `mpMath1`
    Add: `mpMath1`, simplified code

--- mpMath1.py
+++ mpMath1.py
@@ -1,5 +1,17 @@
 from mpmath import *
+from math import exp
 
+"""
+#Q1. how to display the error in mpMath
+#Q2. how to calculate the error, in Trapizodal Integration method ?
+
+#Q3. how can we verify which function is closer to the real (trap or Quad), true `value` of approximation?
+#a. Use verification method, perhaps?
+
+"""
+###########################
+# MISC. ROUTINES #
+###########################
 
 def isIndexValid(x, arr, margin=0):
     """ checks if index is valid """ 
@@ -53,11 +65,6 @@ def handleTuple(x):
     return condition
     raise ValueError("ValueError: please recheck input, then retry")
 
-# Demo:
-print("Debug: checkTuple(-1, -1)")
-x = checkTuple((-1, -1)) # -1
-x = handleTuple(x)
-
         
 def diff(xVal,yVal, kernel = lambda x,y: 3*x*y + 2*y - x):
     """ returns the differentiation of a `kernel` """
@@ -73,24 +80,39 @@ def diff(xVal,yVal, kernel = lambda x,y: 3*x*y + 2*y - x):
         res =  diff( kernel , xVal , yVal ) 
     return res
 
-print("f'() = ", diff((0.25, 0.5) ,(0,1)) )
 #Differentiation
 
 def differentiate(kernel =lambda x: x**2 + x ,x=1.0):
+    """ prints lambda , returns lambda """
     print(diff(lambda x: x**2 + x, x) )
     res = f(1.0)
     dres = diff(res)
     
     f = lambda x :  kernel  #(lambda x: x**2 + x)
+    print("f", f)
+    print("dres = ",dres)
+    return dres
 
-    return f
-
-differentiate
-f = lambda x :  kernel(x)
 def f(x):
     """ lambda function of single variable, x """
     #return x * 2
     return x**2 + x
+
+# Demo:
+print("Debug: checkTuple(-1, -1)")
+x = checkTuple((-1, -1)) # -1
+x = handleTuple(x)
+
+
+
+print("f'() = ", diff((0.25, 0.5) ,(0,1)) )
+
+def kernel(x):x: x**2 + x
+# differentiate
+f = lambda x :  kernel(x)
+
+
+
 print("f1 = ",f(1) )
 # res = f(x) # wount be displayed # erroneous
 res = lambda x: x**2 + x #, 1.0   #erroneous
@@ -131,14 +153,17 @@ print("lambda x: x**2 + x, 1.0 = f = ", f(1.0))
 #Integration
 
 
-#set config(uration)s
+#1. set config(uration)s
 mp.dps = 15  ; mp.pretty = True; error=True;
 
 config = [mp, error ]
 
-#Integration
+# Integration
+
+## set interval:
 interval1 = [0, pi]
 print("Integration, using the Quadrature method (quad):")
+
 
 #standard quadrature 
 q = quad(sin, interval1 )
@@ -154,40 +179,9 @@ print(f"type = {type(q2)}" )
 print(f"ae = {q2.ae}")
 print("with error: +/- {q2.ae}")
 
-""" credit: @cyrusgeyer """
-
-def unnamedFunction(lambdas,parameters, max_lmbd = 32):
-    #max_lmbd = 32
-    lmbds = range(1, max_lmbd + 1)      
-    log_moments = []
-    
-    for _lambda in lambdas:
-        log_moment = 0
-        for q, sigma, T in parameters:
-            log_moment += compute_log_moment(q, sigma, T, lmbd)
-            log_moments.append((lmbd, log_moment))
-    eps, delta = get_privacy_spent(log_moments, target_delta=delta)
-    return eps, delta
-
 
 #trapeziodal rule
-from math import exp
 
-#an important lambnda function [Integrand]
-#Integrand: Lambda Function, f 
-f = lambda x: 5 * exp(-2* x) # dx
-
-# This function has lots of Applications, in differeny `Dynamical Systems`
-#(circuits, spring mass systems, ... )
-
-#general formula
-#Int[a , b]  Lambda 
-#Int[0.1 , 1.3] 5 * exp(-2* x) dx
-#Interval
-a = 0.1; b = 1.3; 
-#Q. How to use `Trapezoidal Rule` to find out the approximate value of this integral ?
-#A. `Trapezoidal`: any function, Integratable, from a to b, can be approximated by the formula:
-#     - need for fundamental measure of the integration error
 def trapizoidal(interval):
     """ source: @numericalmethodsguy
         https://www.youtube.com/watch?v=Xo9RzN5OFBo
@@ -206,12 +200,21 @@ def trapizoidal(interval):
     raise
     BaseException("ERROR: Trapezoidal Integration : Unexpected error occured, please recheck input, then try again ")
 
+###########################
+# INTEGRATION    ROUTINES #
+###########################
+
+#general formula
+#Int[a , b]  Lambda 
+#Int[0.1 , 1.3] 5 * exp(-2* x) dx
+#Interval
+a = 0.1; b = 1.3;
+
+#Q. How to use `Trapezoidal Rule` to find out the approximate value of this integral ?
+#A. `Trapezoidal`: any function, Integratable, from a to b, can be approximated by the formula:
+#     - need for fundamental measure of the integration error
+
 intervalAb = [a,b]
-Integration = trapizoidal(intervalAb) #TODO: Iterative (if applicable)
-print(f"Integration(trapizoidal) = {Integration}" )
-
-integrationQuad = quad(f, intervalAb, verbose=True )
-
 def integrateQuadTanhSinh(f, intervalAb , method = 'tanh-sinh', verbose=True):
     
     return quad(f, intervalAb , method = 'tanh-sinh', verbose = verbose)
@@ -222,9 +225,25 @@ def integrateQuadGaussLegendre(f, intervalAb , verbose=True):
     return quad(f, intervalAb , method = 'gauss-legendre', verbose=verbose)
     #return quadgl(f, intervalAb )
 
-res1 = integrateQuadGaussLegendre(f, intervalAb)
+intervalAb = [0,1]
+#quad(f, [0,1], method='tanh-sinh')  # Good
+#quad(f, intervalAb , method = 'gauss-legendre', verbose=True)
+#res1 = integrateQuadGaussLegendre(f, intervalAb)
 #print(res1.epsilon)
 
+
+#an important lambnda function [Integrand]
+#Integrand: Lambda Function, f 
+f = lambda x: 5 * exp(-2* x) # dx
+
+# This function has lots of Applications, in differeny `Dynamical Systems`
+#(circuits, spring mass systems, ... )
+
+
+Integration = trapizoidal(intervalAb) #TODO: Iterative (if applicable)
+print(f"Integration(trapizoidal) = {Integration}" )
+
+integrationQuad = quad(f, intervalAb, verbose=True )
 
                     
 print("IntegrateQuadGaussLegendre = ",integrateQuadGaussLegendre(f, intervalAb ))
@@ -234,196 +253,3 @@ print("quadgl = ",quadgl(f, intervalAb ))
 
 
 print(f"Integration(Quad) = {integrationQuad}" )
-#1. how to display the error in mpMath
-#2. hwo to calculate the error, in Trapizodal Integration method ?
-
-#q3. how can we verify which function is closer to the real (trap or Quad), true `value` of approximation?
-#a. Use verification method, perhaps?
-
-
-###########################
-# MULTIPRECISION ROUTINES #
-###########################
-
-
-def pdf_gauss_mp(x, sigma, mean):
-  return mp.mpf(1.) / mp.sqrt(mp.mpf("2.") * sigma ** 2 * mp.pi) * mp.exp(
-      - (x - mean) ** 2 / (mp.mpf("2.") * sigma ** 2))
-
-
-def integral_inf_mp(fn):
-  integral, _ = mp.quad(fn, [-mp.inf, mp.inf], error=True)
-  return integral
-
-
-def integral_bounded_mp(fn, lb, ub):
-  integral, _ = mp.quad(fn, [lb, ub], error=True)
-  return integral
-
-
-def distributions_mp(sigma, q):
-  mu0 = lambda y: pdf_gauss_mp(y, sigma=sigma, mean=mp.mpf(0))
-  mu1 = lambda y: pdf_gauss_mp(y, sigma=sigma, mean=mp.mpf(1))
-  mu = lambda y: (1 - q) * mu0(y) + q * mu1(y)
-  return mu0, mu1, mu
-
-def compute_a_mp(sigma, q, lmbd, verbose=False):
-
-  lmbd_int = int(math.ceil(lmbd))
-  condition = lmbd_int == 0
-  if condition:
-    return 1.0
-  elif not condition:
-        return  lmbd_int
-  else: raise ValueError
-
-
-def compute_b_mp(sigma, q, lmbd, verbose=False):
-  lmbd_int = int(math.ceil(lmbd))
-  if lmbd_int == 0:
-    return 1.0
-
-# __repr__
-
-#DEMO 1
-def demo():
-
-    a_lambda = integral_inf_mp(a_lambda_fn)
-    a_lambda_first_term = integral_inf_mp(a_lambda_first_term_fn)
-    a_lambda_second_term = integral_inf_mp(a_lambda_second_term_fn)
-
-    if verbose:
-        print( "A: by numerical integration {} = {} + {}".format(
-        a_lambda,
-        (1 - q) * a_lambda_first_term,
-        q * a_lambda_second_term) ) 
-
-    return _to_np_float64(a_lambda)
-
-# DEMO2:
-def demo2():
-    mu0, _, mu = distributions_mp(sigma, q)
-
-    b_lambda_fn = lambda z: mu0(z) * (mu0(z) / mu(z)) ** lmbd_int
-    b_lambda = integral_inf_mp(b_lambda_fn)
-
-    m = sigma ** 2 * (mp.log((2 - q) / (1 - q)) + 1 / (2 * (sigma ** 2)))
-    b_fn = lambda z: ((mu0(z) / mu(z)) ** lmbd_int -
-                    (mu(-z) / mu0(z)) ** lmbd_int)
-    if verbose:
-        print ("M =", m)
-        print ("f(-M) = {} f(M) = {}".format(b_fn(-m), b_fn(m)) ) 
-    assert b_fn(-m) < 0 and b_fn(m) < 0
-
-# DEMO3:
-def demo3():
-    b_lambda_int1_fn = lambda z: mu0(z) * (mu0(z) / mu(z)) ** lmbd_int
-    b_lambda_int2_fn = lambda z: mu0(z) * (mu(z) / mu0(z)) ** lmbd_int
-    b_int1 = integral_bounded_mp(b_lambda_int1_fn, -m, m)
-    b_int2 = integral_bounded_mp(b_lambda_int2_fn, -m, m)
-    a_lambda_m1 = compute_a_mp(sigma, q, lmbd - 1)
-    b_bound = a_lambda_m1 + b_int1 - b_int2
-
-    if verbose:
-        print("B by numerical integration", b_lambda)
-        print("B must be no more than    ", b_bound)
-        assert b_lambda < b_bound + 1e-5
-    return _to_np_float64(b_lambda)
-
-# _compute_delta
-def _compute_delta(log_moments, eps):
-  """Compute delta for given log_moments and eps.
-  Args:
-    log_moments: the log moments of privacy loss, in the form of pairs
-      of (moment_order, log_moment)
-    eps: the target epsilon.
-  Returns:
-    delta
-  """
-  min_delta = 1.0
-  for moment_order, log_moment in log_moments:
-    if moment_order == 0:
-      continue
-    if math.isinf(log_moment) or math.isnan(log_moment):
-      sys.stderr.write("The %d-th order is inf or Nan\n" % moment_order)
-      continue
-    if log_moment < moment_order * eps:
-      min_delta = min(min_delta,
-                      math.exp(log_moment - moment_order * eps))
-  return min_delta
-
-# _compute_eps
-def _compute_eps(log_moments, delta):
-  """Compute epsilon for given log_moments and delta.
-  Args:
-    log_moments: the log moments of privacy loss, in the form of pairs
-      of (moment_order, log_moment)
-    delta: the target delta.
-  Returns:
-    epsilon
-  """
-  min_eps = float("inf")
-  for moment_order, log_moment in log_moments:
-    if moment_order == 0:
-      continue
-    if math.isinf(log_moment) or math.isnan(log_moment):
-      sys.stderr.write("The %d-th order is inf or Nan\n" % moment_order)
-      continue
-    min_eps = min(min_eps, (log_moment - math.log(delta)) / moment_order)
-  return min_eps
-
-
-def compute_log_moment(q, sigma, steps, lmbd, verify=False, verbose=False):
-  """Compute the log moment of Gaussian mechanism for given parameters.
-  Args:
-    q: the sampling ratio.
-    sigma: the noise sigma.
-    steps: the number of steps.
-    lmbd: the moment order.
-    verify: if False, only compute the symbolic version. If True, computes
-      both symbolic and numerical solutions and verifies the results match.
-    verbose: if True, print out debug information.
-  Returns:
-    the log moment with type np.float64, could be np.inf.
-  """
-  moment = compute_a(sigma, q, lmbd, verbose=verbose)
-  momentnp = np.inf
-  if verify:
-    mp.dps = 50
-    moment_a_mp = compute_a_mp(sigma, q, lmbd, verbose=verbose)
-    moment_b_mp = compute_b_mp(sigma, q, lmbd, verbose=verbose)
-    np.testing.assert_allclose(moment, moment_a_mp, rtol=1e-10)
-    if not np.isinf(moment_a_mp):
-      # The following test fails for (1, np.inf)!
-      np.testing.assert_array_less(moment_b_mp, moment_a_mp)
-  if np.isinf(moment):
-    return momentnp #np.inf
-  else:
-    momentnp = np.log(moment) * steps
-    return momentnp #np.log(moment) * steps
-
-def get_privacy_spent(log_moments, target_eps=None, target_delta=None):
-  """Compute delta (or eps) for given eps (or delta) from log moments.
-  Args:
-    log_moments: array of (moment_order, log_moment) pairs.
-    target_eps: if not None, the epsilon for which we would like to compute
-      corresponding delta value.
-    target_delta: if not None, the delta for which we would like to compute
-      corresponding epsilon value. Exactly one of target_eps and target_delta
-      is None.
-  Returns:
-    eps, delta pair
-  """
-# Test
-##assert
-
-## assert (target_eps is None) ^ (target_delta is None)
-## assert not ((target_eps is None) and (target_delta is None))
-
-def testTargetEps(target_eps,log_moments,target_delta):
-    if target_eps is not None:
-        return (target_eps, _compute_delta(log_moments, target_eps))
-    else:
-        return (_compute_eps(log_moments, target_delta), target_delta)
-
-
